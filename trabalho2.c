e0737c0055de9ba5654b6ee5dd90a1c10db49887
João Pedro Canabarro <joaopedrocanabarro20@gmail.com>
2026-08-24 16:07:17 -0300

    falta tudo apenas

--- trabalho2.c
+++ trabalho2.c
@@ -3,6 +3,21 @@
 #include <string.h>
 
 
-int main(){
+void preenche_ident(int qtd, int* identeq){
+    for (int i = 0; i < qtd; i++)
+    {
+        /* code */
+    }
     
+}
+
+int main(){
+    int quantidade_equipes, quantidade_etapas;
+    int* identeq;
+    printf("Insira a quantidade de equipes:\n");
+    scanf(" %d", &quantidade_equipes);
+    printf("Insira a quantidade de etaps:\n");
+    scanf(" %d", &quantidade_etapas);
+    identeq = (int*)malloc(sizeof(int)*quantidade_equipes);
+    preenche_ident(quantidade_equipes, identeq);
 }
