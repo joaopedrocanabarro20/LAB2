6d19f96b962f86a9fd9c33f4f9dcd7fb51896fd8
João Pedro Canabarro <joaopedrocanabarro20@gmail.com>
2026-08-24 23:41:05 -0300

    arrumar malloc maluco

--- trabalho2.c
+++ trabalho2.c
@@ -2,22 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
+void preenche_pontos(int qtde, int etapas, int** pontos){
 
-void preenche_ident(int qtd, int* identeq){
-    for (int i = 0; i < qtd; i++)
+    for (int i = 0; i < qtde; i++)
     {
-        /* code */
+        for (int j = 0; j < etapas; j++)
+        {
+            scanf("%d", &pontos[i][j]);
+        }
+        
     }
     
+}
+
+void preenche_ident(int qtd, int* identeq){
+    printf("Digite o codigo de identificação das %d equipes\n", qtd);
+    for (int i = 0; i < qtd; i++)
+    {
+        scanf("%d", &*(identeq+i));
+    }
 }
 
 int main(){
     int quantidade_equipes, quantidade_etapas;
     int* identeq;
+    int** pontuacoes;
     printf("Insira a quantidade de equipes:\n");
     scanf(" %d", &quantidade_equipes);
-    printf("Insira a quantidade de etaps:\n");
+    printf("Insira a quantidade de etapas:\n");
     scanf(" %d", &quantidade_etapas);
     identeq = (int*)malloc(sizeof(int)*quantidade_equipes);
     preenche_ident(quantidade_equipes, identeq);
+    pontuacoes=(int**)malloc(sizeof(int*)*quantidade_equipes*quantidade_etapas);
+    preenche_pontos(quantidade_equipes, quantidade_etapas, pontuacoes);
 }
+//corrigir o malloc
