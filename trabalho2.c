4a1594c14643a304be39a68f84f232b88ba8bbed
João Pedro Canabarro <joaopedrocanabarro20@gmail.com>
2026-09-02 18:47:40 -0300

    atualizando

--- trabalho2.c
+++ trabalho2.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
+
+int calcula_totais(int **pontuacoes, int qtd_et, int i){
+    int total=0;
+    for (int j=0; j<qtd_et; j++){
+        total+=pontuacoes[i][j];
+    }
+    return total;
+}
+float calcula_media(int **pontuacoes, int qtd_et, int i){
+    int total = calcula_totais(pontuacoes, qtd_et, i);
+    return (float)total/qtd_et;
+}
+void tabelageral(int *identeq, int **pontuacoes, int qtd_eq, int qtd_et){
+    printf("|EQUIPE(S)|");
+    for (int i=0; i<qtd_et; i++){
+        printf("|ETAPA %d|", i+1);
+    }
+    printf("|TOTAL|");
+    printf("|MEDIA|\n");
+    for(int i=0; i<qtd_eq; i++){
+       printf("|   %d   |", identeq[i]);
+       for(int j=0; j<qtd_et; j++){
+        printf("|   %d   |", pontuacoes[i][j]);
+       }
+       printf("|  %d  |", calcula_totais(pontuacoes, qtd_et, i ));
+       printf("| %.3f |\n", calcula_media(pontuacoes, qtd_et, i));
+    }
+}
 void alocarVetores(int qtd, int** ident){
     *ident = (int*)malloc(sizeof(int)*qtd);
     if (*ident == NULL)
@@ -19,7 +48,7 @@ void alocarMatrizes(int qtdequipe, int qtdetapas, int ***pontuacoes){
 
 }
 
-void preenche_pontos(int qtde, int etapas, int** pontos, int *ident){
+void cadastro_pontuacoes(int qtde, int etapas, int** pontos, int *ident){
     printf("\n\n==Digite a pontuação das equipes==\n\n");
     for (int i = 0; i < qtde; i++)
     {
@@ -31,8 +60,6 @@ void preenche_pontos(int qtde, int etapas, int** pontos, int *ident){
             scanf("%d", &pontos[i][j]);
         }
     }
-
-    
 }
 
 void preenche_ident(int qtd, int* identeq){
@@ -44,25 +71,16 @@ void preenche_ident(int qtd, int* identeq){
 }
 
 int main(){
-    int quantidade_equipes, quantidade_etapas, opc;
+    int quantidade_equipes, quantidade_etapas, opc, verifica=0;
     int* identeq;
     int** pontuacoes;
-    printf("Insira a quantidade de equipes:\n");
-    scanf(" %d", &quantidade_equipes);
-    printf("Insira a quantidade de etapas:\n");
-    scanf(" %d", &quantidade_etapas);
-    alocarVetores(quantidade_equipes, &identeq);
-    preenche_ident(quantidade_equipes, identeq);
-    alocarMatrizes(quantidade_equipes, quantidade_etapas, &pontuacoes);
     
-    preenche_pontos(quantidade_equipes, quantidade_etapas, pontuacoes, identeq);
-
     do
     {
         printf("\n===== MENU =====\n");
-        printf("1 - Opcao 1\n");
-        printf("2 - Opcao 2\n");
-        printf("3 - Opcao 3\n");
+        printf("1 - Cadastrar Identificação e pontuação das equipes\n");
+        printf("2 - Exibir Tabela Geral\n");
+        printf("3 - Exibir Classificação Final\n");
         printf("4 - Opcao 4\n");
         printf("0 - Sair\n");
         printf("Escolha: ");
@@ -72,12 +90,38 @@ int main(){
         switch (opc)
         {
         case 1:
+
+            if(verifica==0){
+            printf("Insira a quantidade de equipes:\n");
+            scanf(" %d", &quantidade_equipes);
+            printf("Insira a quantidade de etapas:\n");
+            scanf(" %d", &quantidade_etapas);
+            alocarVetores(quantidade_equipes, &identeq);
+            alocarMatrizes(quantidade_equipes, quantidade_etapas, &pontuacoes);
+            preenche_ident(quantidade_equipes, identeq);
+            cadastro_pontuacoes(quantidade_equipes, quantidade_etapas, pontuacoes, identeq);
+
+            verifica++;
+            }
+            else{
+                printf("Você ja preencheu !!\n");
+                sleep(3); //pesquisei sobre essa função, basicamente ela espera um pouco para aparecer o menu dnv
+            }
             break;
         case 2:
-            
+
+        if (verifica == 0)
+        {
+            printf("Você precisa preencher !!\n");
+            sleep(3);
+        }
+        else{
+            tabelageral(identeq, pontuacoes, quantidade_equipes, quantidade_etapas);
+            sleep(5);
+        }
             break;
         case 3:
-
+        Exibir_classificacao_final();
             break;
         case 4:
 
